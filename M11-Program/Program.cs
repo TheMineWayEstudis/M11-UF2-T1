7429e6b0b90a1054b458469897b1922a191b8a25
Joel Campos <joelcamposoliva9000@gmail.com>
2021-12-02 21:17:36 +0100

    Program initial commit

---
+++ M11-Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M11_Program
+{
+    /*
+     * PROGRAMA CREAT PER L'ASSIGNATURA D'M11
+     * Com estic una mica boig i m'agrada programar coses he fet una sol·lució una mica més àmplia
+     * 
+     * Repositori públic: https://github.com/TheMineWayEstudis/M11-UF2-T1
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
