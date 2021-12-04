b51e0fb986cf5b80f15ecaedd46a3e0c3b95e92d
Joel Campos <joelcamposoliva9000@gmail.com>
2021-12-04 12:38:16 +0100

    Exer 7

--- M11-Program/Components/Screens/Exer7.cs
+++ M11-Program/Components/Screens/Exer7.cs
@@ -1,14 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace M11_Program.Components.Screens
 {
     class Exer7: IScreen
     {
+        const string DEFAULT_NAME = "Olaf";
+        const string DEFAULT_PASSWORD = "Count123";
         public void open()
         {
+            Console.Clear();
+            // Es carreguen les credencials simulades (per defecte Olaf i Count123)
+            string name = UIBasics.readText($"Nom d'usuari ({DEFAULT_NAME})", DEFAULT_NAME);
+            string password = UIBasics.readText($"Contrasenya ({DEFAULT_PASSWORD})", DEFAULT_PASSWORD);
+            int delay = UIBasics.readNumber($"Delay (buit o 0 per deshabil·litar el delay)", 0);
 
+            // Es carrega el fitxer de diccionari
+            FileInfo file = UIBasics.getFile();
+            if (file == null) return;
+
+            // Llegim el contingut del fitxer
+            string[] passwords = File.ReadAllText(file.FullName).Split('\n');
+
+            Console.WriteLine();
+            UIBasics.displayLine();
+            Console.WriteLine();
+
+            // Realitzem l'intent d'atac mitjançant un bucle paral·lel
+            string match = null;
+            Parallel.ForEach(passwords, pass =>
+            {
+                Console.WriteLine($"Provant contrasenya {pass}");
+
+                // Aplica el delay
+                Thread.Sleep(delay * 1000);
+
+                if (password == pass.Trim())
+                {
+                    match = pass;
+                    return;
+                }
+            });
+
+            Console.WriteLine();
+            UIBasics.displayLine();
+            Console.WriteLine();
+
+            if (match == null) Console.WriteLine("No s'han trobat coincidències");
+            else Console.WriteLine($"S'ha trobat una coincidència: {match}");
+
+            UIBasics.pressAnyKey();
         }
     }
 }
--- M11-Program/Components/UIBasics.cs
+++ M11-Program/Components/UIBasics.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace M11_Program.Components
 {
@@ -9,6 +8,57 @@ namespace M11_Program.Components
         public static void displayLine()
         {
             for (int i = 0; i < Console.WindowWidth; i++) Console.Write("*");
+        }
+
+        public static string readText(string label, string defaultValue = null)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.Write($"{label}: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string val = Console.ReadLine();
+            Console.ForegroundColor = color;
+            return val == "" ? defaultValue : val;
+        }
+
+        public static int readNumber(string label, int defaultValue = 0)
+        {
+            try
+            {
+                return int.Parse(readText(label));
+            } catch(System.Exception e)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static bool getYesNo(string label)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.Write($"{label} (Y/N): ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            bool option = Console.ReadKey().Key == ConsoleKey.Y;
+            Console.ForegroundColor = color;
+            Console.WriteLine();
+            return option;
+        }
+
+        public static FileInfo getFile()
+        {
+            string name = readText("Escriu el nom del fitxer (buit per sortir)");
+            if (name == "") return null;
+
+            if(!File.Exists(Directory.GetCurrentDirectory() + @"\" + name))
+            {
+                return null;
+            }
+
+            return new FileInfo(Directory.GetCurrentDirectory() + @"\" + name);
+        }
+
+        public static void pressAnyKey()
+        {
+            Console.WriteLine("Prem qualsevol tecla per continuar...");
+            Console.ReadKey();
         }
     }
 }
