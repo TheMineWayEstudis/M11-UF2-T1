ae29714dbc7650a4e84d96644a191de2255e7554
Joel Campos <joelcamposoliva9000@gmail.com>
2021-12-05 14:43:29 +0100

    Exer 8

--- M11-Program/Components/Screens/Exer8.cs
+++ M11-Program/Components/Screens/Exer8.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace M11_Program.Components.Screens
 {
     class Exer8: IScreen
     {
-        public void open() { }
+        public void open() {
+            Console.Clear();
+            Console.WriteLine("Indica el fitxer que vols replicar.");
+            FileInfo file = UIBasics.getFile();
+            if (file == null) return;
+
+            for(int i = 0; i < 10; i++)
+            {
+                File.Copy(file.FullName, Directory.GetCurrentDirectory() + $@"\{$"Còpia {i + 1} " + file.Name}");
+            }
+
+            Console.WriteLine("\nS'ha replicat el fitxer");
+            UIBasics.pressAnyKey();
+        }
     }
 }
